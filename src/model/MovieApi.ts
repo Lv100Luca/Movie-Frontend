dc08ee067e58905e88fd44c2b1e0b13789f09cc1
Luca Diegel <Luca@trenz.de>
2023-04-20 17:25:31 +0200

    added all endpoints

--- src/model/MovieApi.ts
+++ src/model/MovieApi.ts
@@ -3,6 +3,14 @@ import Movie from "@/model/Movie";
 export default class MovieApi {
     private static readonly url = "http://localhost:5091/MovieApi"
 
+    private static readJsonList(jsonData: any): any[] {
+        const list: Movie[] = [];
+        for (let jsonDataKey in jsonData) {
+            list.push(new Movie(jsonData[jsonDataKey]["name"], jsonData[jsonDataKey]["id"]))
+        }
+        return list;
+    }
+
     public static async ApiGet(): Promise<Movie[]> { //todo is this o to do so?
         const response = await fetch(this.url, {
             method: "GET",
@@ -10,15 +18,11 @@ export default class MovieApi {
                 "Content-Type": "application/json",
             },
         });
-        const jsonData = await response.json();
-        const listOfMovies: Movie[] = [];
-        for (let jsonDataKey in jsonData) {
-            listOfMovies.push(new Movie(jsonData[jsonDataKey]["name"], jsonData[jsonDataKey]["id"]))
-        }
-        return listOfMovies;
+        const jsonData = await response.json(); //ask -> how to properly read JsonData in Js
+        return this.readJsonList(jsonData);
     }
 
-    public static async ApiPost(name: string) {
+    public static async ApiPost(name: string) { //todo append this to list directly so you dont have to get again
         const response = await fetch(this.url, {
             method: "POST",
             headers: {
@@ -33,26 +37,50 @@ export default class MovieApi {
         }
     }
 
-    public static async ApiGetByName(name: string): Promise<Movie[]> {
+    public static async ApiGetByName(name: string): Promise<Movie[]> { // todo return empty list if 404
         const response = await fetch(this.url + "/name/" + name, {
             method: "get",
             headers: {
                 "Content-Type": "application/json",
             }
         });
-        const jsonData = await response.json();
-        console.log(jsonData);
-        const listOfMovies: Movie[] = [];
-        for (let jsonDataKey in jsonData) {
-            listOfMovies.push(new Movie(jsonData[jsonDataKey]["name"], jsonData[jsonDataKey]["id"]))
+        if (response.ok) {
+            const jsonData = await response.json();
+            return this.readJsonList(jsonData);
+        } else {
+            return [];
         }
-        return listOfMovies;
     }
 
 
     public static async ApiGetById(id: number) {
-        const response = await fetch(this.url)
+        console.log("ID:" + id)
+        const response = await fetch(this.url + "/id/" + id.toString(), {
+            method: "get",
+            headers: {
+                "Content-Type": "application/json",
+            }
+        });
+        // ask -> JSON.parse(); nicht nötig?
+        const movieData = await response.json();
+        console.log("Returning Movie " + id)
+        return new Movie(movieData["name"], movieData["id"]);
+    }
 
+    public static async ApiDeleteById(id: number) {
+        console.log(id);
+        const response = await fetch(this.url + "/id/" + id, {
+            method: "delete",
+            headers: {
+                "Content-Type": "application/json",
+            }
+        });
+        console.log("response: " + response.status)
+        if (response.ok) {
+            console.log("204 is okayge")
+        } else {
+            console.log("204 is not okayge")
+        }
     }
 
     // async function sendEmail(dataURL: string) {
@@ -85,7 +113,7 @@ export default class MovieApi {
     //         }
     //
     //         return true;
-    //     }
 
+    //     }
     // }
 }
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,8 +6,12 @@ import type Movie from "@/model/Movie";
 
 const listOfMovies = ref<Movie[]>([]);
 const listOfMoviesByName = ref<Movie[]>([]);
-const movieName = ref<string>("");
+const movieNamePost = ref<string>("");
 const movieNameGet = ref<string>("");
+const movieId = ref<number>();
+const movieById = ref<Movie>();
+
+const movieDelete = ref();
 
 onMounted(() => {
     getMovies();
@@ -21,7 +25,7 @@ function getMovies() { //todo rename to updateMovies and run onMounted
 function addMovie(name: string) {
     console.log("Posting");
     MovieApi.ApiPost(name).then(getMovies);
-    movieName.value = "";
+    movieNamePost.value = "";
     // getMovies();
 }
 
@@ -32,7 +36,12 @@ function getMovieByName(name: string) {
 
 function getMovieById(id: number) {
     console.log("getByID: " + id);
-    MovieApi.ApiGetById(id);
+    MovieApi.ApiGetById(id).then(movie => movieById.value = movie);
+}
+
+function deleteMovieByName(id: number) {
+    console.log("deleteByName: " + id);
+    MovieApi.ApiDeleteById(id).then(getMovies);
 }
 
 </script>
@@ -51,24 +60,45 @@ function getMovieById(id: number) {
             </div>
             <div class="movie-post">
                 <div class=movie-post-request>
-                    <input @keydown.enter="addMovie(movieName)" v-model="movieName" placeholder="Name:" type="text"
+                    <input @keydown.enter="addMovie(movieNamePost)" v-model="movieNamePost" placeholder="Name of Movie!"
+                           type="text"
                            class="movie-post-text-input">
-                    <button class="movie-post-button" @click="addMovie(movieName)">add Movie</button>
+                    <button :disabled="(movieNamePost == '')" class="movie-post-button"
+                            @click="addMovie(movieNamePost)">add Movie
+                    </button>
                 </div>
                 <!--                <label class="movie-post-response-code">-->
                 <!--                    code-->
                 <!--                </label>-->
             </div>
             <div class=movie-get-by-Name>
-                <input type="text" v-model="movieNameGet" placeholder="Name:">
-                <button @click="getMovieByName(movieNameGet)">get Movie</button>
-                <div class="movie-get-wrapper">
+                <div>
+                    <input type="text" v-model="movieNameGet" placeholder="Search By Name:">
+                    <button :disabled="movieNameGet == ''" @click="getMovieByName(movieNameGet)">get Movie</button>
+                </div>
+                <div v-show="listOfMoviesByName.length != 0" class="movie-get-wrapper">
                     <div v-for="movieName in listOfMoviesByName">
                         <label class="movie-get-label">{{ movieName }}</label>
                     </div>
                 </div>
             </div>
+            <div class="movie-get-by-id">
+                <div>
+                    <input type="text" v-model="movieId" placeholder="Search By Id:">
+                    <button :disabled="movieId == undefined || movieId == ''" @click="getMovieById(movieId!)">get
+                        Movie
+                    </button>
+                </div>
+                <label>{{ movieById }}</label>
+            </div>
+            <div class=movie-get-by-Name>
+                <div style="display: flex;">
+                    <input type="text" v-model="movieDelete" placeholder="Delete by Id:">
+                    <button :disabled="movieDelete == undefined || movieDelete == ''" @click="deleteMovieByName(movieDelete)">Delete!</button>
+                </div>
+            </div>
         </div>
+
     </div>
 </template>
 <style>
@@ -84,6 +114,12 @@ function getMovieById(id: number) {
     display: flex;
     flex-direction: column;
     gap: 1em;
+}
+
+.body > * {
+    display: flex;
+    flex-direction: column;
+    width: 100%;
 }
 
 .movie-get {

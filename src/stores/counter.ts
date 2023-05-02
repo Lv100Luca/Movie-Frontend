47bdfa8c102a8126decbb36bdd0e80c982bd5398
Luca Diegel <Luca@trenz.de>
2023-05-02 09:43:38 +0200

    added all endpoints

--- src/stores/counter.ts
+++ src/stores/counter.ts
@@ -1,8 +1,7 @@
-import { ref, computed } from 'vue'
-import { defineStore } from 'pinia'
+import {defineStore} from 'pinia'
 
 export const useCounterStore = defineStore('counter', () => {
 
 
-  return {  }
+    return {}
 })
